66bb789c1de15c2159b6516ee427157e5ac89981
FrozenMushroom <3855063604@qq.com>
2025-02-10 15:04:53 +0800

    Add files via upload

---
+++ Audio recognition-predict.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+import os
+import re
+import shutil
+import string
+import pathlib
+from tensorflow.keras import layers
+from tensorflow.keras import losses
+import numpy as np
+import seaborn as sns
+import tensorflow as tf
+from IPython import display
+
+data_dir = pathlib.Path("C:/PYTHONDATA/Audio recognition/data/mini_speech_commands_extracted/mini_speech_commands/").with_suffix('')
+
+train_ds, val_ds = tf.keras.utils.audio_dataset_from_directory(
+    directory=data_dir,
+    batch_size=64,
+    validation_split=0.2,
+    seed=0,
+    output_sequence_length=16000,
+    subset='both')
+
+label_names = np.array(train_ds.class_names)
+def squeeze(audio, labels):
+  audio = tf.squeeze(audio, axis=-1)
+  return audio, labels
+
+train_ds = train_ds.map(squeeze, tf.data.AUTOTUNE)
+val_ds = val_ds.map(squeeze, tf.data.AUTOTUNE)
+
+test_ds = val_ds.shard(num_shards=2, index=0)
+val_ds = val_ds.shard(num_shards=2, index=1)
+
+for example_audio, example_labels in train_ds.take(1):
+    break
+
+def get_spectrogram(waveform):
+  # Convert the waveform to a spectrogram via a STFT.
+  spectrogram = tf.signal.stft(
+      waveform, frame_length=255, frame_step=128)
+  # Obtain the magnitude of the STFT.
+  spectrogram = tf.abs(spectrogram)
+  # Add a `channels` dimension, so that the spectrogram can be used
+  # as image-like input data with convolution layers (which expect
+  # shape (`batch_size`, `height`, `width`, `channels`).
+  spectrogram = spectrogram[..., tf.newaxis]
+  return spectrogram
+
+def make_spec_ds(ds):
+  return ds.map(
+      map_func=lambda audio,label: (get_spectrogram(audio), label),
+      num_parallel_calls=tf.data.AUTOTUNE)
+
+train_spectrogram_ds = make_spec_ds(train_ds)
+val_spectrogram_ds = make_spec_ds(val_ds)
+test_spectrogram_ds = make_spec_ds(test_ds)
+
+for example_spectrograms, example_spect_labels in train_spectrogram_ds.take(1):
+  break
+
+train_spectrogram_ds = train_spectrogram_ds.cache().shuffle(10000).prefetch(tf.data.AUTOTUNE)
+val_spectrogram_ds = val_spectrogram_ds.cache().prefetch(tf.data.AUTOTUNE)
+test_spectrogram_ds = test_spectrogram_ds.cache().prefetch(tf.data.AUTOTUNE)
+
+input_shape = example_spectrograms.shape[1:]
+print('Input shape:', input_shape)
+num_labels = len(label_names)
+
+# Instantiate the `tf.keras.layers.Normalization` layer.
+norm_layer = layers.Normalization()
+# Fit the state of the layer to the spectrograms
+# with `Normalization.adapt`.
+norm_layer.adapt(data=train_spectrogram_ds.map(map_func=lambda spec, label: spec))
+
+new_model = tf.keras.models.load_model('my_model.keras')
+
+loss, accuracy = new_model.evaluate(test_spectrogram_ds)
+
+print("Loss: ", loss)
+print("Accuracy: ", accuracy)
---
+++ Audio recognition.py
@@ -0,0 +1,106 @@
+import matplotlib.pyplot as plt
+import os
+import re
+import shutil
+import string
+import pathlib
+from tensorflow.keras import layers
+from tensorflow.keras import losses
+import numpy as np
+import seaborn as sns
+import tensorflow as tf
+from IPython import display
+
+data_dir = pathlib.Path("C:/PYTHONDATA/Audio recognition/data/mini_speech_commands_extracted/mini_speech_commands/").with_suffix('')
+
+train_ds, val_ds = tf.keras.utils.audio_dataset_from_directory(
+    directory=data_dir,
+    batch_size=64,
+    validation_split=0.2,
+    seed=0,
+    output_sequence_length=16000,
+    subset='both')
+
+label_names = np.array(train_ds.class_names)
+def squeeze(audio, labels):
+  audio = tf.squeeze(audio, axis=-1)
+  return audio, labels
+
+train_ds = train_ds.map(squeeze, tf.data.AUTOTUNE)
+val_ds = val_ds.map(squeeze, tf.data.AUTOTUNE)
+
+test_ds = val_ds.shard(num_shards=2, index=0)
+val_ds = val_ds.shard(num_shards=2, index=1)
+
+for example_audio, example_labels in train_ds.take(1):
+    break
+
+def get_spectrogram(waveform):
+  # Convert the waveform to a spectrogram via a STFT.
+  spectrogram = tf.signal.stft(
+      waveform, frame_length=255, frame_step=128)
+  # Obtain the magnitude of the STFT.
+  spectrogram = tf.abs(spectrogram)
+  # Add a `channels` dimension, so that the spectrogram can be used
+  # as image-like input data with convolution layers (which expect
+  # shape (`batch_size`, `height`, `width`, `channels`).
+  spectrogram = spectrogram[..., tf.newaxis]
+  return spectrogram
+
+def make_spec_ds(ds):
+  return ds.map(
+      map_func=lambda audio,label: (get_spectrogram(audio), label),
+      num_parallel_calls=tf.data.AUTOTUNE)
+
+train_spectrogram_ds = make_spec_ds(train_ds)
+val_spectrogram_ds = make_spec_ds(val_ds)
+test_spectrogram_ds = make_spec_ds(test_ds)
+
+for example_spectrograms, example_spect_labels in train_spectrogram_ds.take(1):
+  break
+
+train_spectrogram_ds = train_spectrogram_ds.cache().shuffle(10000).prefetch(tf.data.AUTOTUNE)
+val_spectrogram_ds = val_spectrogram_ds.cache().prefetch(tf.data.AUTOTUNE)
+test_spectrogram_ds = test_spectrogram_ds.cache().prefetch(tf.data.AUTOTUNE)
+
+input_shape = example_spectrograms.shape[1:]
+print('Input shape:', input_shape)
+num_labels = len(label_names)
+
+# Instantiate the `tf.keras.layers.Normalization` layer.
+norm_layer = layers.Normalization()
+# Fit the state of the layer to the spectrograms
+# with `Normalization.adapt`.
+norm_layer.adapt(data=train_spectrogram_ds.map(map_func=lambda spec, label: spec))
+
+model = tf.keras.Sequential([
+    layers.Input(shape=input_shape),
+    # Downsample the input.
+    layers.Resizing(32, 32),
+    # Normalize.
+    norm_layer,
+    layers.Conv2D(32, 3, activation='relu'),
+    layers.Conv2D(64, 3, activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Dropout(0.25),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.5),
+    layers.Dense(num_labels),
+])
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'],
+)
+
+EPOCHS = 10
+history = model.fit(
+    train_spectrogram_ds,
+    validation_data=val_spectrogram_ds,
+    epochs=EPOCHS,
+    callbacks=tf.keras.callbacks.EarlyStopping(verbose=1, patience=2),
+)
+
+model.save('my_model.keras')
